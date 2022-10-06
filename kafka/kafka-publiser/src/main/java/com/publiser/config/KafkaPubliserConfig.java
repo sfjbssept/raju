d2a90dbaff50df11eaf71bb0523bd0e01fcef7e4
sfjbssept <sfjbssept@gmail.com>
2022-10-06 03:39:15 +0000

    kafka consumer

--- kafka/kafka-publiser/src/main/java/com/publiser/config/KafkaPubliserConfig.java
+++ kafka/kafka-publiser/src/main/java/com/publiser/config/KafkaPubliserConfig.java
@@ -16,24 +16,18 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 public class KafkaPubliserConfig {
 	
 	@Bean
-	public ProducerFactory<String, Object> producerFactory(){
-		
-		Map<String,Object> config=new HashMap<String,Object>();
-		
-		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-		return new DefaultKafkaProducerFactory<String,Object>(config);
+	public ProducerFactory<String, Object> producerFactory() {
+		Map<String, Object> configs = new HashMap<>();
+		configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		return new DefaultKafkaProducerFactory<String , Object>(configs);
 	}
 	
 	@Bean
-	KafkaTemplate<String, Object> kafkaTemplete(){
+	public KafkaTemplate<String, Object> kafkaTemplate() {
 		return new KafkaTemplate<>(producerFactory());
 	}
-	
-	
-	
-	
 	
 
 }
---
+++ school-management-microservice/kafka-consumer/src/main/java/com/consumer/config/KafkaConsumerConfig.java
@@ -0,0 +1,61 @@
+package com.consumer.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import com.consumer.entity.User;
+
+@Configurable
+@EnableKafka
+public class KafkaConsumerConfig {
+	
+	
+
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory() {
+		Map<String, Object> configs = new HashMap<>();
+		configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		configs.put(ConsumerConfig.GROUP_ID_CONFIG,"groupid-1");
+		return new DefaultKafkaConsumerFactory<>(configs);
+	}
+	
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+		factory.setConsumerFactory(consumerFactory());;
+		return factory;
+	}
+	
+	
+	// config for our json object 
+	@Bean
+	public ConsumerFactory<String, User> userConsumerFactory() {
+		Map<String, Object> configs = new HashMap<>();
+		configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+		configs.put(ConsumerConfig.GROUP_ID_CONFIG,"groupid-2");
+		return new DefaultKafkaConsumerFactory<>(configs, new StringDeserializer(), new JsonDeserializer<>(User.class));
+	
+	}
+	
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, User> userKafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, User> factory = new ConcurrentKafkaListenerContainerFactory<String, User>();
+		factory.setConsumerFactory(userConsumerFactory());;
+		return factory;
+	}
+	
+}
