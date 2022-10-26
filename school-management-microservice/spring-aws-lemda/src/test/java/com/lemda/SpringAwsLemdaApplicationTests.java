ccdbea36a95034db3d55702fdf8a446adf009e7e
sfjbssept <sfjbssept@gmail.com>
2022-10-26 07:20:37 +0000

    added the other file

---
+++ school-management-microservice/spring-aws-lemda/src/test/java/com/lemda/SpringAwsLemdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsLemdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
